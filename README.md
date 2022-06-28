46700409f268ad2630af602e227ac7ee7efe10f1
gokul007007 <gokulgowda18@gmail.com>
2022-06-28 20:25:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-joystick-11ntuz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-joystick-11ntuz)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Joystick from "./Joystick";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Joystick!</h1>
+    
+      <div>
+      <Joystick />
+      </div>
+    </div>
+  );
+}
